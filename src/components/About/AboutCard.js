ba94913a338676be3694ec804a11fd6cdd316fac
Tirth Jain <jaintirth2705@gmail.com>
2025-12-20 23:20:19 +0530

    Updated the resume, techstack and toolstack

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -20,7 +20,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> GYM
             </li>
             <li className="about-activity">
               <ImPointRight /> Sport Buzzer
