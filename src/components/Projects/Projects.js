a4a945df1a5c1d80c4bc91e41b1544342115855f
Tirth Jain <jaintirth2705@gmail.com>
2025-12-24 23:31:09 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import journallite from "../../Assets/Projects/journallite.png";
+import unistop from "../../Assets/Projects/unistop.png";
+import prediction from "../../Assets/Projects/prediction.png";
+import mealAgent from "../../Assets/Projects/mealAgent.png";
+import projectSeries from "../../Assets/Projects/projectSeries.png";
 
 function Projects() {
   return (
@@ -23,68 +23,55 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={journallite}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Modern Journaling"
+              description="Journalling platform for students to share their thoughts and feelings. It generates the insights based on the entries. It also detetcts the mood of the user and suggests the appropriate content."
+              ghLink="https://github.com/Jain-Tirth/Journalite"
+              demoLink="https://journalite-a1327.web.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={unistop}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="UniStop"
+              description="Platform for the students to find the competition from various platform and collects it under single platfrom Unistop. It is created to save the time of the students to find the competition."
+              ghLink="https://github.com/Jain-Tirth/OpportuneX"
+              demoLink="https://unistop.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={prediction}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Disease Prediction System"
+              description="Disease prediction system predicts the disease based on the symptoms entered by the user. It is created to help the user to find the disease based on the symptoms."
+              ghLink="https://github.com/Jain-Tirth/Disease-Prediction-System"
+              demoLink="https://disease-prediction-system-one.vercel.app/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={mealAgent}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Meal Planner Agent"
+              description="Created a meal planner agent which plans the meal for the user based on the user's preference. It also helps the user to find the recipe for the meal and also checks if the budget provided by the user is sufficient or not."
+              ghLink="https://github.com/NikzRN01/Agent"
+              demoLink="https://www.youtube.com/watch?si=I3RdzEsWlKU0yDTB&v=dLqA-BjQf0E&feature=youtu.be"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={projectSeries}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="21 Days ML, DL and GenAI Project Series"
+              description="All in combine 21 project series on Machine Learning, Deep Learning and Generative AI. It showscase the hunger to learn about the machine learning, deep learning, and the Generative AI."
+              ghLink="https://github.com/Jain-Tirth/21-Days-Project-Series"
             />
           </Col>
         </Row>
